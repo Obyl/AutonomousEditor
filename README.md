aae0e380a019d925a16bc33fd218d69726f18d59
Obyl <oliverbyl@yahoo.com>
2018-07-15 19:53:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Autonomous Editor
+This program aims to provide an easy way to create instructions for autonomous robots.
---
+++ src/ca/team4152/autoeditor/Main.java
@@ -0,0 +1,9 @@
+package ca.team4152.autoeditor;
+
+public class Main {
+
+    public static void main(String[] args){
+
+    }
+
+}
