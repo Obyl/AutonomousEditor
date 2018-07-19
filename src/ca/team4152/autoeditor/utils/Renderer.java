c13a7a6ffb462608299abd3666eab00cc6c47e87
Obyl <oliverbyl@yahoo.com>
2018-07-19 14:00:20 -0400

    Revised Bresenham's line algorithm

--- src/ca/team4152/autoeditor/utils/Renderer.java
+++ src/ca/team4152/autoeditor/utils/Renderer.java
@@ -152,79 +152,67 @@ public class Renderer {
             int x1 = (int) ((scale * nextNode.getX()) + xScroll - xOrigin) + xOrigin;
             int y1 = (int) ((scale * nextNode.getY()) + yScroll - yOrigin) + yOrigin;
 
-            if(Math.abs(y1 - y0) < Math.abs(x1 - x0)){
-                if(x0 > x1){
-                    plotLineLow(x1, y1, x0, y0);
-                }else{
-                    plotLineLow(x0, y0, x1, y1);
-                }
-            }else{
-                if(y0 > y1){
-                    plotLineHigh(x1, y1, x0, y0);
-                }else{
-                    plotLineHigh(x0, y0, x1, y1);
-                }
+            boolean steep = false;
+
+            if(Math.abs(x0 - x1) < Math.abs(y0 - y1)){
+                int mid = x0;
+                x0 = y0;
+                y0 = mid;
+
+                mid = x1;
+                x1 = y1;
+                y1 = mid;
+
+                steep = true;
             }
-        }
-    }
 
-    private void plotLineLow(int x0, int y0, int x1, int y1){
-        double dx = x1 - x0;
-        double dy = y1 - y0;
-        int yi = 1;
-        if(dy < 0){
-            yi = -1;
-            dy = -dy;
-        }
-        double D = 2*dy - dx;
-        int y = y0;
-        for(int x = x0; x < x1; x++){
-            for(int p = 0; p < 9; p++){
-                int xp = x + (p % 3);
-                int yp = y + (p / 3);
+            if(x0 > x1){
+                int mid = x0;
+                x0 = x1;
+                x1 = mid;
 
-                if(xp >= 0 && xp < editor.getWindowWidth() &&
-                        yp >= 0 && yp < editor.getWindowHeight() &&
-                        (pixels[xp + yp * editor.getWindowWidth()] == FIELD_BACKGROUND ||
-                        pixels[xp + yp * editor.getWindowWidth()] == COLLISION_BOX_FILL)){
-                    pixels[xp + yp * editor.getWindowWidth()] = PATH_NODE_FILL;
+                mid = y0;
+                y0 = y1;
+                y1 = mid;
+            }
+
+            int xChange = x1 - x0;
+            int yChange = y1 - y0;
+
+            int changeError = Math.abs(yChange) * 2;
+            int changeError2 = 0;
+
+            int y = y0;
+            for(int x = x0; x < x1; x++){
+                for(int p = 0; p < 9; p++){
+                    int xp = x + (p % 3);
+                    int yp = y + (p / 3);
+
+                    if(xp >= 0 && xp < editor.getWindowWidth() && yp >= 0 && yp < editor.getWindowHeight()){
+                        if(steep){
+                            int arrayIndex = yp + xp * editor.getWindowWidth();
+                            int currentColor = pixels[arrayIndex];
+
+                            if(currentColor == FIELD_BACKGROUND || currentColor == COLLISION_BOX_FILL){
+                                pixels[arrayIndex] = PATH_NODE_FILL;
+                            }
+                        }else{
+                            int arrayIndex = xp + yp * editor.getWindowWidth();
+                            int currentColor = pixels[arrayIndex];
+
+                            if(currentColor == FIELD_BACKGROUND || currentColor == COLLISION_BOX_FILL){
+                                pixels[arrayIndex] = PATH_NODE_FILL;
+                            }
+                        }
+                    }
                 }
-            }
-            if(D > 0){
-                y = y + yi;
-                D = D - 2*dx;
-            }
-            D = D + 2*dy;
-        }
-    }
 
-    private void plotLineHigh(int x0, int y0, int x1, int y1){
-        double dx = x1 - x0;
-        double dy = y1 - y0;
-        int xi = 1;
-        if(dx < 0){
-            xi = -1;
-            dx = -dx;
-        }
-        double D = 2*dx - dy;
-        int x = x0;
-        for(int y = y0; y < y1; y++){
-            for(int p = 0; p < 9; p++){
-                int xp = x + (p % 3);
-                int yp = y + (p / 3);
-
-                if(xp >= 0 && xp < editor.getWindowWidth() &&
-                        yp >= 0 && yp < editor.getWindowHeight() &&
-                        (pixels[xp + yp * editor.getWindowWidth()] == FIELD_BACKGROUND ||
-                        pixels[xp + yp * editor.getWindowWidth()] == COLLISION_BOX_FILL)){
-                    pixels[xp + yp * editor.getWindowWidth()] = PATH_NODE_FILL;
+                changeError2 += changeError;
+                if(changeError2 > xChange){
+                    y += (y1 > y0 ? 1 : -1);
+                    changeError2 -= (xChange * 2);
                 }
             }
-            if(D > 0){
-                x = x + xi;
-                D = D - 2*dy;
-            }
-            D = D + 2*dx;
         }
     }
 
