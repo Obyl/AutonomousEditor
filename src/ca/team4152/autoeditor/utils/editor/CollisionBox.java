d884077ff4648a1c5f2bd6d9310c85bdb2cc7107
Obyl <oliverbyl@yahoo.com>
2018-10-21 13:21:36 -0400

    Improve dragging and resizing of collision boxes

--- src/ca/team4152/autoeditor/utils/editor/CollisionBox.java
+++ src/ca/team4152/autoeditor/utils/editor/CollisionBox.java
@@ -6,10 +6,10 @@ public class CollisionBox extends EditorNode{
     private int oldMouseY = -1;
     private int xMidpoint;
     private int yMidpoint;
-    private boolean dragging;
-    private int dragMode;
-    private boolean inCorner;
-    private boolean inSide;
+    private boolean changeX0;
+    private boolean changeY0;
+    private boolean changeX1;
+    private boolean changeY1;
 
     public CollisionBox(int x0, int y0, int x1, int y1) {
         super(x0, y0, x1, y1);
@@ -19,18 +19,18 @@ public class CollisionBox extends EditorNode{
 
     @Override
     public boolean intersects(int x, int y) {
-        if(isSelected()){
-            //If it's selected, extend the corners and side midpoints a little bit to make it clear where you need to click.
-            inCorner = (x >= getX0() - 3 && x < getX0() + 3 && y >= getY0() - 3 && y < getY0() + 3) ||
-                    (x >= getX1() - 3 && x < getX1() + 3 && y >= getY0() - 3 && y < getY0() + 3) ||
-                    (x >= getX0() - 3 && x < getX0() + 3 && y >= getY1() - 3 && y < getY1() + 3) ||
-                    (x >= getX1() - 3 && x < getX1() + 3 && y >= getY1() - 3 && y < getY1() + 3);
-            inSide = (x >= xMidpoint - 3 && x < xMidpoint + 3 && y >= getY0() - 3 && y < getY0() + 3) ||
-                    (x >= getX1() - 3 && x < getX1() + 3 && y >= yMidpoint - 3 && y < yMidpoint + 3) ||
-                    (x >= xMidpoint - 3 && x < xMidpoint + 3 && y >= getY1() - 3 && y < getY1() + 3) ||
-                    (x >= getX0() - 3 && x < getX0() + 3 && y >= yMidpoint - 3 && y < yMidpoint + 3);
-        }
-        return inCorner || inSide || (x >= getX0() && x < getX1() && y >= getY0() && y < getY1());
+        //If it's selected, extend the points where you can grab.
+        boolean extend = isSelected() && (
+                (x >= getX0() - 3 && x < getX0() + 3 && y >= getY0() - 3 && y < getY0() + 3) ||
+                (x >= getX1() - 3 && x < getX1() + 3 && y >= getY0() - 3 && y < getY0() + 3) ||
+                (x >= getX0() - 3 && x < getX0() + 3 && y >= getY1() - 3 && y < getY1() + 3) ||
+                (x >= getX1() - 3 && x < getX1() + 3 && y >= getY1() - 3 && y < getY1() + 3) ||
+                (x >= xMidpoint - 3 && x < xMidpoint + 3 && y >= getY0() - 3 && y < getY0() + 3) ||
+                (x >= getX1() - 3 && x < getX1() + 3 && y >= yMidpoint - 3 && y < yMidpoint + 3) ||
+                (x >= xMidpoint - 3 && x < xMidpoint + 3 && y >= getY1() - 3 && y < getY1() + 3) ||
+                (x >= getX0() - 3 && x < getX0() + 3 && y >= yMidpoint - 3 && y < yMidpoint + 3)
+                );
+        return extend || (x >= getX0() && x < getX1() && y >= getY0() && y < getY1());
     }
 
     @Override
@@ -38,51 +38,49 @@ public class CollisionBox extends EditorNode{
         if(newDrag){
             oldMouseX = x;
             oldMouseY = y;
-            dragging = false;
-            dragMode = 1;
+
+            //Check which values we should change depending on where you grabbed.
+            //Only do this when you start a new drag so that the points don't mess
+            //with each other when they overlap.
+            changeX0 = x >= getX0() - 3 && x < getX0() + 3;
+            changeX1 = x >= getX1() - 3 && x < getX1() + 3;
+            changeY0 = y >= getY0() - 3 && y < getY0() + 3;
+            changeY1 = y >= getY1() - 3 && y < getY1() + 3;
         }
 
+        //Store current values.
         int x0 = getX0();
         int y0 = getY0();
         int x1 = getX1();
         int y1 = getY1();
 
-        //Tell the code which values to set depending on where the mouse is.
-        //Encode and decode these values using small prime numbers.
-        if(dragging || inCorner || inSide){
-            dragging = true;
+        //Flip minimum and maximum x values if necessary.
+        //(always keep x0 < x1)
+        if(x1 < x0) {
+            int mid = x1;
+            x1 = x0;
+            x0 = mid;
 
-            if(inCorner){
-                if(Math.abs(x0 - x) < Math.abs(x1 - x))
-                    dragMode *= 2;
-                else
-                    dragMode *= 3;
+            changeX0 = !changeX0;
+            changeX1 = !changeX1;
+        }
+        //Flip minimum and maximum y values if necessary.
+        //(always keep y0 < y1)
+        if(y1 < y0) {
+            int mid = y1;
+            y1 = y0;
+            y0 = mid;
 
-                if(Math.abs(y0 - y) < Math.abs(y1 - y))
-                    dragMode *= 5;
-                else
-                    dragMode *= 7;
-            }
+            changeY0 = !changeY0;
+            changeY1 = !changeY1;
+        }
 
-            if(inSide){
-                if(x >= xMidpoint - 3 && x < xMidpoint + 3 && y >= getY0() - 3 && y < getY0() + 3)
-                    dragMode *= 5;
-                else if(x >= getX1() - 3 && x < getX1() + 3 && y >= yMidpoint - 3 && y < yMidpoint + 3)
-                    dragMode *= 3;
-                else if(x >= xMidpoint - 3 && x < xMidpoint + 3 && y >= getY1() - 3 && y < getY1() + 3)
-                    dragMode *= 7;
-                else if(x >= getX0() - 3 && x < getX0() + 3 && y >= yMidpoint - 3 && y < yMidpoint + 3)
-                    dragMode *= 2;
-            }
-
-            if(dragMode % 2 == 0)
-                x0 = x;
-            if(dragMode % 3 == 0)
-                x1 = x;
-            if(dragMode % 5 == 0)
-                y0 = y;
-            if(dragMode % 7 == 0)
-                y1 = y;
+        //Change values as necessary.
+        if(changeX0 || changeY0 || changeX1 || changeY1){
+            x0 = changeX0 ? x : x0;
+            y0 = changeY0 ? y : y0;
+            x1 = changeX1 ? x : x1;
+            y1 = changeY1 ? y : y1;
         }else{
             int xInc = x - oldMouseX;
             int yInc = y - oldMouseY;
@@ -93,39 +91,13 @@ public class CollisionBox extends EditorNode{
             y1 += yInc;
         }
 
-        if(x1 < x0){
-            int mid = x1;
-            x1 = x0;
-            x0 = mid;
-
-            if(dragMode % 2 == 0){
-                dragMode /= 2;
-                dragMode *= 3;
-            }else{
-                dragMode /= 3;
-                dragMode *= 2;
-            }
-        }
-        if(y1 < y0){
-            int mid = y1;
-            y1 = y0;
-            y0 = mid;
-
-            //Reverse drag modes.
-            if(dragMode % 5 == 0){
-                dragMode /= 5;
-                dragMode *= 7;
-            }else{
-                dragMode /= 7;
-                dragMode *= 5;
-            }
-        }
-
+        //Apply changed values.
         setX0(x0);
         setY0(y0);
         setX1(x1);
         setY1(y1);
 
+        //Update the old mouse position and midpoints of box.
         oldMouseX = x;
         oldMouseY = y;
         updateMidpoints();
